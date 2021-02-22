a8b0b88a38c0bc0b0a2435146dac10f452d849c9
iseelise <39001717+elisepitel@users.noreply.github.com>
2021-02-22 06:13:42 +0100

    Update scoreboard.py
    add higher score logic

--- scoreboard.py
+++ scoreboard.py
@@ -2,22 +2,31 @@ from turtle import Turtle
 ALIGNEMENT = 'center'
 FONT = ("Courier", 18, "normal")
 
+
 class ScoreBoard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.goto(0, 270)
         self.color('white')
-        self.write(f"🧚 Score: {self.score}", align=ALIGNEMENT, font=FONT)
+        self.write(f"🧚 Score: {self.score} High Score: {self.high_score}", align=ALIGNEMENT, font=FONT)
         self.hideturtle()
 
     def update_score(self):
         self.clear()
-        self.score += 1
-        self.write(f"🧚 Score: {self.score}", align=ALIGNEMENT, font=FONT)
+        self.write(f"🧚 Score: {self.score} High Score: {self.high_score}", align=ALIGNEMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"Game Over", align=ALIGNEMENT, font=("Courier", 22, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.update_score()
+        self.score = 0
+
+    def increase_score(self):
+        self.score += 1
